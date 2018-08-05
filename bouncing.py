020586b7d7046c9ad0101d83d261e8bdce98a100
shypre <shyprea2@gmail.com>
2018-08-05 16:19:37 -0700

    Add bouncing.py: bouncing LED across strip

---
+++ bouncing.py
@@ -0,0 +1,27 @@
+from np_simplelib import *
+
+
+FOREGROUND = (0,   255, 0, INTENSITY)
+BACKGROUND = (255, 255, 255, int(INTENSITY/2))
+MIN_DISTANCE = 8
+
+"""r, g, b, a = 0, 0, 0, 0
+while True:
+    r = (r + 2) % 256
+    g = (g + 7) % 256
+    b = (b + 9) % 256
+    a = (a + 10) % 256
+    r, g, b = rgb_normalize(r, g, b)
+    color = (r, g, b, a)
+    set_all(color)
+    
+    np.show()
+    time.sleep(DELAY)
+"""
+set_all(BACKGROUND)
+while True:
+	for n in range(NUM_PIXELS):
+		np.setPixelColorRGB((n-1+255)%255, *BACKGROUND)
+		np.setPixelColorRGB(n, *FOREGROUND)
+		np.show()
+		time.sleep(DELAY)
