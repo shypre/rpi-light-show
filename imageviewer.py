b781d4356f9e96beb1216ac4fa9d31cf50c395f5
James Lu <james@overdrivenetworks.com>
2018-12-26 22:32:48 -0800

    imageviewer.py: project images onto an LED matrix
    Squashed commit of the following:
    
    commit 6c92a894bd4087a5f33e51390cf6d74beece76fa
    Author: James Lu <james@overdrivenetworks.com>
    Date:   Wed Dec 26 22:14:49 2018 -0800
    
        Rename Adafruit_NeoPixel -> PixelStrip
    
    commit b5d4325f983c6593b17cb6bacae8f619a4e375c1
    Author: James Lu <james@overdrivenetworks.com>
    Date:   Wed Dec 26 22:14:14 2018 -0800
    
        imageviewer: call np.begin() before drawing
    
    commit 47f8735606977b7ce538c7f5ae8cc9c3cae49d2d
    Author: James Lu <james@overdrivenetworks.com>
    Date:   Wed Dec 26 21:37:14 2018 -0800
    
        Use rpi_ws281x instead of neopixel
    
    commit b54891a2d433590069abde5ca067a0038c15f21b
    Author: James Lu <james@overdrivenetworks.com>
    Date:   Wed Dec 26 21:16:34 2018 -0800
    
        WIP: An image viewer for the LED matrix

---
+++ imageviewer.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Projects an image on to a LED matrix via GPIO.
+"""
+
+import argparse
+from PIL import Image
+
+LED_INTENSITY = 25
+
+def process_image(filename, size):
+    im = Image.open(filename)
+    out = im.resize((size, size))
+    return out
+
+def draw_led_matrix(np, image):
+    x = y = 0
+    for num, pixel in enumerate(image.getdata()):
+        # Tracking x, y is only for debug purposes
+        print("Drawing pixel %s\tat (%d, %d)" % (pixel, x, y))
+
+        np.setPixelColorRGB(num, *pixel)
+
+        if x >= (image.width - 1):
+            y += 1
+            x = 0
+        else:
+            x += 1
+    np.show()
+
+def main():
+    parser = argparse.ArgumentParser(description='Project an image onto a LED matrix.')
+    parser.add_argument('filename', type=str, help='filename of the image to display')
+    parser.add_argument('boardsize', type=int, help='the width and height of the LED matrix')
+    parser.add_argument('gpionum', type=int, help='the target GPIO pin')
+
+    args = parser.parse_args()
+    try:
+        from rpi_ws281x import PixelStrip
+    except ImportError:
+        class PixelStrip():
+            """PixelStrip stub."""
+            def __init__(self, *args, **kwargs):
+                print("rpi_ws281x not found - this won't actually draw anything.")
+
+            def begin(self):
+                pass
+
+            def setPixelColorRGB(self, *args):
+                pass
+
+            def show(self):
+                print("Done mock drawing.")
+
+    np = PixelStrip(args.boardsize**2, args.gpionum, brightness=LED_INTENSITY)
+    np.begin()
+
+    image = process_image(args.filename, args.boardsize)
+    draw_led_matrix(np, image)
+
+if __name__ == '__main__':
+    main()
