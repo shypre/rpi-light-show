2fc1958c2141b4b77ac31a721f0bf2421e5e6b60
James Lu <james@overdrivenetworks.com>
2018-12-29 03:03:46 -0800

    imageviewer: draw properly using the serpentine pattern

--- imageviewer.py
+++ imageviewer.py
@@ -5,7 +5,10 @@ Projects an image on to a LED matrix via GPIO.
 
 import argparse
 from PIL import Image
+import traceback
+
 import terminal_output
+from simplegrid import *
 
 LED_INTENSITY = 25
 
@@ -14,21 +17,19 @@ def process_image(filename, size):
     out = im.resize((size, size))
     return out
 
-def draw_led_matrix(np, image):
+def draw_led_matrix(grid, image):
     x = y = 0
     for num, pixel in enumerate(image.getdata()):
         # Tracking x, y is only for debug purposes
         print("Drawing pixel %s\tat (%d, %d)" % (pixel, x, y))
 
-        np.setPixelColorRGB(num, *pixel)
+        grid.set(x, y, pixel)
 
         if x >= (image.width - 1):
             y += 1
             x = 0
         else:
             x += 1
-    terminal_output.print_led_grid_np(np._led_data, 16, 16)
-    np.show()
 
 def main():
     parser = argparse.ArgumentParser(description='Project an image onto a LED matrix.')
@@ -57,8 +58,15 @@ def main():
     np = PixelStrip(args.boardsize**2, args.gpionum, brightness=LED_INTENSITY)
     np.begin()
 
+    my_grid = led_grid.LEDGrid(np, grid.SerpentinePattern.TOP_RIGHT, args.boardsize, args.boardsize)
+
     image = process_image(args.filename, args.boardsize)
-    draw_led_matrix(np, image)
+    draw_led_matrix(my_grid, image)
+    np.show()
+    try:
+        terminal_output.print_led_grid(my_grid)
+    except:
+        traceback.print_exc()
 
 if __name__ == '__main__':
     main()
